1a52cffa0970606b0c46de6f12a7304ccfcb51a5
Bruno Gusmão <brunogusmao43@gmail.com>
2023-02-09 18:03:50 -0300

    building the menu bar

---
+++ src/components/menu-bar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {MenuBarContainer} from './styles';
+function MenuBar() {
+    return (
+        <MenuBarContainer>
+            <menubar_container>ola ola</menubar_container>
+        </MenuBarContainer>
+    )
+}
+
+export default MenuBar;
---
+++ src/components/menu-bar/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MenuBarContainer = styled.div`
+    menubar_container {
+        background-color: #fafafa;
+        display: flex;
+        flex-direction: row;
+        height: 80px;
+
+    }
+    
+`
