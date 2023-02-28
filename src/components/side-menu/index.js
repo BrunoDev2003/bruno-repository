51c84775f8ad5d58e9225cd38b6a6146f2974dbb
Bruno Gusmão <brunogusmao43@gmail.com>
2023-02-28 17:03:29 -0300

    implementing the base of the side-menu

--- src/components/side-menu/index.js
+++ src/components/side-menu/index.js
@@ -6,10 +6,27 @@ import Icon from '@mui/material/Icon';
 export default function SideMenu() {
     return (
         <SideMenuContainer>
-            <div className="container">
+            <div className="sidemenu-container">
                 <ul>
-                    <li>Categorias</li>
+                    <h1>Categorias</h1>
+                    <li>Acessórios</li>
+                    <li>Aniversário e Festas</li>
+                    <li>Bebê</li>
+                    <li>Bijuterias</li>
+                    <li>Bolsas e Carteiras</li>
+                    <li>Casa</li>
+                    <li>Casamento</li> 
+                    <li>Convites</li>
                 </ul>
+            </div>
+            <div className="sidemenu-container_2">
+            <ul>
+                    <h1>Materiais para artesanato</h1>
+                    <li>Bijuterias e acessórios</li>
+                    <li>Papel e scrapbooking</li>
+                    <li>Patchwork e costura</li>
+                </ul>
+                <button>Ver todos</button>
             </div>
         </SideMenuContainer>
     )
--- src/components/side-menu/styles.js
+++ src/components/side-menu/styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
 export const SideMenuContainer = styled.div`
-
+    .sidemenu-container {
+        display: flex;
+        align-items: center;
+        font-size: .7rem;
+    }
+    .sidemenu-container_2 {
+        display: flex;
+        align-items: center;
+        font-size: .7rem;
+        justify-content:flex-start;
+    }
+    li {
+        color: #C2E0F7;
+    }
+    button {
+        display:flex;
+        align-items:center;
+    }
 `
