2b5062eaffa3363f04428e0f0e25b3a2ae3039ff
Bruno Gusmão <brunogusmao43@gmail.com>
2023-03-02 16:54:12 -0300

    implementing the stories-menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import  MenuBar  from './components/menu-bar/';
 import SideMenu from './components/side-menu';
+import StoriesMenu from './components/stories-menu';
 
 function App() {
   return (
     <div className="App">
       <MenuBar></MenuBar>
       <SideMenu></SideMenu>
+      <StoriesMenu></StoriesMenu>
     </div>
   );
 }
---
+++ src/components/stories-menu/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {StoriesMenuContainer} from './styles';
+
+
+export default function StoriesMenu() {
+
+
+    return(
+        <div className="storiesmenu-container">
+            <picture>Imagem aniversário elo4</picture>
+            
+        </div>
+    )
+}
