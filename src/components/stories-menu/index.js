a98fbb55a6ecce258108d3d59dd83f0a6912c445
Bruno Gusmão <brunogusmao43@gmail.com>
2023-03-03 17:55:06 -0300

    working on the base of the stories-menu

--- src/components/stories-menu/index.js
+++ src/components/stories-menu/index.js
@@ -6,9 +6,24 @@ export default function StoriesMenu() {
 
 
     return(
-        <div className="storiesmenu-container">
-            <picture>Imagem aniversário elo4</picture>
-            
+
+        <div className="products-container">
+            <div className="storiesmenu-container">
+                <picture>Imagem aniversário elo4</picture>
+            </div>
+            <div className="stories-sortable-container">
+                <div className="stories-wrap-container">
+                    <div className="stories-image-container">
+                        <picture>
+                            <a href="#" className="story-link"></a>
+                        </picture>
+                    </div>
+                    <div className="stories-editable-text-container">
+                        Texto deve ser passado por prop
+                    </div>
+                </div>
+            </div>
         </div>
+        
     )
 }
