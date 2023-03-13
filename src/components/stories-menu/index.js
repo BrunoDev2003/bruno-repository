233ba023e9df07f89c2da13320e6533c931fb46e
Bruno Gusmão <brunogusmao43@gmail.com>
2023-03-13 17:51:29 -0300

    adding the first story-image in the style-component

--- src/components/stories-menu/index.js
+++ src/components/stories-menu/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {StoriesMenuContainer} from './styles';
 import Elo7Birthday from '../../../src/assets/imgs/elo7Img.png';
+import PascoaImg from '../../../src/assets/imgs/PascoaImg.jpg';
 
 
 export default function StoriesMenu() {
@@ -28,10 +29,18 @@ export default function StoriesMenu() {
                 <div className="stories-wrap-container">
                     <div className="stories-image-container">
                         <picture>
-                        
-                            <div className="story-2">
+                        <StoriesMenuContainer>
+                            <div className="story-2" data-override-name="true">
+                            <img 
+                                className="story2-image"
+                                src={PascoaImg}
+                                    
+                            >
+                                    
+                            </img>
 
                             </div>
+                        </StoriesMenuContainer>
                             <div className="story-3">
                                 
                             </div>
--- src/components/stories-menu/styles.js
+++ src/components/stories-menu/styles.js
@@ -13,4 +13,12 @@ export const StoriesMenuContainer = styled.div`
         margin-right: 40rem;
         width:100%;
     }
+
+    .story2-image  {
+        display: flex;
+        flex-direction: row;
+        border-radious: 1px solid #FDB933;
+        width: 30%;
+        height: 30%;
+    }
 `
